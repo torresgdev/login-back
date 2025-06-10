65419a5700088a003f3c47592df9c54996957a0c
Gabriel Torres <torresg.dev@gmail.com>
2025-06-10 08:17:43 -0300

    prepared db docker, updated app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USERNAME', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', '123'),
+        database: configService.get<string>('DB_DATABASE', 'auth_db'),
+        entities: [__dirname + '/**/*.entity{.ts,.js'],
+
+        synchronize: true,
+
+        autoLoadEntities: true,
+        logging:['query', 'error']
+      }),
+
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
