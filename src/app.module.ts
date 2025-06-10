3694ec8675c64f135e0bcf451601c76a5ffa117d
Gabriel Torres <torresg.dev@gmail.com>
2025-06-10 08:28:08 -0300

    created entity user, update user.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -27,7 +28,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       }),
 
       inject: [ConfigService]
-    })
+    }),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
