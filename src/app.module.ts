9a424dad8bd9ea514aaa8e98544061eec7387424
Gabriel Torres <torresg.dev@gmail.com>
2025-06-10 10:30:13 -0300

    update app.module, main.ts, tested

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
@@ -20,7 +21,7 @@ import { AuthModule } from './auth/auth.module';
         username: configService.get<string>('DB_USERNAME', 'postgres'),
         password: configService.get<string>('DB_PASSWORD', '123'),
         database: configService.get<string>('DB_DATABASE', 'auth_db'),
-        entities: [__dirname + '/**/*.entity{.ts,.js'],
+        entities: [User],
 
         synchronize: true,
 
